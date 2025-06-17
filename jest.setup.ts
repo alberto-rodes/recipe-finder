f29c99fe7a57e3b1455e9105c052faf7f84fc8bc
alberto-rodes <alberto.rodes.garcia@gmail.com>
2025-06-17 18:24:01 +0200

    fix tests

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,9 @@
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import { TextEncoder, TextDecoder } from "util";
+
+Object.assign(global, { TextDecoder, TextEncoder });
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: jest.fn(),
+}));
